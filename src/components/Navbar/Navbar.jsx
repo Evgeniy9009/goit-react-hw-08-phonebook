893cadc1af81d5edbba63b02c29d422cc078041b
Evgeniy9009 <evgeniy.grabar1@gmail.com>
2022-11-12 17:54:20 +0200

    delete Link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import NavbarMenu from "./NavbarMenu/NavbarMenu";
 import NavbarAuth from './NavbarAuth/NavbarAuth';
